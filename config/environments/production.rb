847e48d9d3d1a0426b2de25553446b273fa43894
Eko <eko@eko-lenovo.(none)>
2013-10-30 20:20:19 +0200

    other fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,4 @@ Todo::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  RAILS_ENV=production bundle exec rake assets:precompile
 end
